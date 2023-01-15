9b4108de0b01c0d4a23abe1796dffc07bdca245a
Fabian Schnötzinger <fabian.schnoetzinger@outlook.com>
2023-01-15 18:31:00 +0100

    TextAnalyzer wurde implementiert und so umgeändert, dass es mit der RESTful Applikation zusammenpasst

--- codingTask_Dedalus/src/main/java/com/example/codingtask_dedalus/CodingTaskDedalusController.java
+++ codingTask_Dedalus/src/main/java/com/example/codingtask_dedalus/CodingTaskDedalusController.java
@@ -1,7 +1,10 @@
 package com.example.codingtask_dedalus;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class CodingTaskDedalusController {
@@ -9,5 +12,15 @@ public class CodingTaskDedalusController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/analyzeVovels/{text}")
+    public HashMap<String, Integer> analyzeVovels(@PathVariable("text") String text) {
+        return new TextAnalyzer().analyzeVovels(text);
+    }
+
+    @GetMapping("/analyzeConsonants/{text}")
+    public HashMap<String, Integer> analyzeConsonants(@PathVariable("text") String text) {
+        return new TextAnalyzer().analyzeConsonants(text);
     }
 }
--- codingTask_Dedalus/src/main/java/com/example/codingtask_dedalus/TextAnalyzer.java
+++ codingTask_Dedalus/src/main/java/com/example/codingtask_dedalus/TextAnalyzer.java
@@ -14,63 +14,63 @@ import java.util.HashMap;
 
 public class TextAnalyzer {
 
-  public static void main(String[] args) {
-    String input = "";
+  public HashMap<String, Integer> analyzeVovels(String text){
+    HashMap<String, Integer> vovels = new HashMap<>();
+
     int numA = 0;
     int numE = 0;
     int numI = 0;
     int numO = 0;
     int numU = 0;
-    if (args[0].equals("vowels")) {
-      input = args[1];
-      char[] chars = input.toCharArray();
-      for (int i = 0; i < chars.length; i++) {
-        System.out.println(chars[i]);
-        if (chars[i] == 'a' || chars[i] == 'A')
-          numA++;
-        if (chars[i] == 'e' || chars[i] == 'E')
-          numE++;
-        if (chars[i] == 'i' || chars[i] == 'I')
-          numI++;
-        if (chars[i] == 'o' || chars[i] == 'O')
-          numO++;
-        if (chars[i] == 'u' || chars[i] == 'U')
-          numU++;
-      }
-      System.out.println("Letter 'A' appears " + numA + " times");
-      System.out.println("Letter 'E' appears " + numE + " times");
-      System.out.println("Letter 'I' appears " + numI + " times");
-      System.out.println("Letter 'O' appears " + numO + " times");
-      System.out.println("Letter 'U' appears " + numU + " times");
-    } else if (args[0].equals("consonants")) {
-      input = args[1];
-      HashMap<String, Integer> consonants = new HashMap<>();
-      char[] chars = input.toCharArray();
-      for (int i = 0; i < chars.length; i++) {
-        if (chars[i] != 'a'
-          && chars[i] != 'A'
-          && chars[i] != 'e'
-          && chars[i] != 'E'
-          && chars[i] != 'i'
-          && chars[i] != 'I'
-          && chars[i] != 'o'
-          && chars[i] != 'O'
-          && chars[i] != 'u'
-          && chars[i] != 'U'
-        ) {
-          String stringCharacter = String.valueOf(chars[i]).toUpperCase();
-          if (consonants.containsKey(stringCharacter)) {
-            Integer num = consonants.get(stringCharacter);
-            num++;
-            consonants.put(stringCharacter, num);
-          } else{
-            consonants.put(stringCharacter, 1);
-          }
+    char[] chars = text.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] == 'a' || chars[i] == 'A')
+        numA++;
+      if (chars[i] == 'e' || chars[i] == 'E')
+        numE++;
+      if (chars[i] == 'i' || chars[i] == 'I')
+        numI++;
+      if (chars[i] == 'o' || chars[i] == 'O')
+        numO++;
+      if (chars[i] == 'u' || chars[i] == 'U')
+        numU++;
+    }
+
+    vovels.put("A", numA);
+    vovels.put("E", numE);
+    vovels.put("I", numI);
+    vovels.put("O", numO);
+    vovels.put("U", numU);
+
+    return vovels;
+  }
+
+  public HashMap<String, Integer> analyzeConsonants(String text) {
+    HashMap<String, Integer> consonants = new HashMap<>();
+    char[] chars = text.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] != 'a'
+              && chars[i] != 'A'
+              && chars[i] != 'e'
+              && chars[i] != 'E'
+              && chars[i] != 'i'
+              && chars[i] != 'I'
+              && chars[i] != 'o'
+              && chars[i] != 'O'
+              && chars[i] != 'u'
+              && chars[i] != 'U'
+      ) {
+        String stringCharacter = String.valueOf(chars[i]).toUpperCase();
+        if (consonants.containsKey(stringCharacter)) {
+          Integer num = consonants.get(stringCharacter);
+          num++;
+          consonants.put(stringCharacter, num);
+        } else {
+          consonants.put(stringCharacter, 1);
         }
       }
-      consonants.entrySet().forEach(entrySet -> {
-        System.out.println("Letter '" + entrySet.getKey() + "' appears " + entrySet.getValue() + " times");
-      });
     }
+
+    return consonants;
   }
 }
